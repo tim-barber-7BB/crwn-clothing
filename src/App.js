a871be317ae26695146fa6c49d756c92c5915ce7
tim <myemail@email.com>
2022-11-14 13:42:36 +0000

    added firebase and added signin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.components'
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/Navigation/navigation.component';
 
@@ -12,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   )
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,48 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAI0v72t4bAoJT3YvhDStM0-SaG6f-CnXU",
+  authDomain: "crwn-db-1b642.firebaseapp.com",
+  projectId: "crwn-db-1b642",
+  storageBucket: "crwn-db-1b642.appspot.com",
+  messagingSenderId: "946021108182",
+  appId: "1:946021108182:web:d35e06fd07fbc404447c77"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  if(!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (err) {
+      console.log('error creating user: ', err);
+    }
+  }
+
+  return userDocRef;
+}
