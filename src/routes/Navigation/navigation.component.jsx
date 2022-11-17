1ebd6b4c7afd4884445b8c8338291ba82421a790
tim <myemail@email.com>
2022-11-17 17:57:53 +0000

    116 toggle cart open

--- src/routes/Navigation/navigation.component.jsx
+++ src/routes/Navigation/navigation.component.jsx
@@ -3,8 +3,9 @@ import { Fragment, useContext } from "react";
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../context/user.context';
-import { signOutUser
- } from "../../utils/firebase/firebase.utils";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -30,7 +31,9 @@ const Navigation = () => {
               : (<Link className="nav-link" to="/sign-in">SIGN IN</Link>
             )
           }
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -9,7 +9,7 @@ const Shop = () => {
   <div className="products-container">
     {products.map( product => (
       <ProductCard key={product.id} product={product} />
-    ))};
+    ))}
   </div>
   )
 };
